eba8a7e5afa1c026e948cd34000bd9291e912634
Truong <r0ck3t.1973@gmail.com>
2026-09-15 15:59:55 +0700

    xog lab1

---
+++ jsnc_fa26-main/lab1.js
@@ -0,0 +1,14 @@
+console.log("Ngày Thứ nhất của kì thử nghiệm");
+
+let name = "trường";
+
+const age = 19;
+let address = "Nam Định";
+let isStudent = true;
+
+console.log("Ten:",name);
+console.log("Tuổi:", age);
+console.log("Địa chỉ:", address);
+console.log("Sinh viên:", isStudent);
+
+
